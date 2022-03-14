f77f2391d84ad885949d9d6fdb3e9f6053c09f33
Steven Ngo <steven.ngo1999@gmail.com>
2022-03-14 02:14:16 +0100

    Ajout tests healer + warrior

--- src/test/com/isep/rpg/hero/HealerTest.java
+++ src/test/com/isep/rpg/hero/HealerTest.java
@@ -1,5 +1,6 @@
 package com.isep.rpg.hero;
 
+import com.isep.rpg.enemy.Enemy;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import com.isep.rpg.hero.Hero;
@@ -7,12 +8,21 @@ import com.isep.rpg.hero.Hero;
 class HealerTest {
 
     @Test
-    void attack(Hero hero) {
+    void heal(Hero hero) {
         Healer healer = new Healer();
         int manaBeforeTurn = healer.manaPoints;
         int HPBeforeTurn = hero.lifePoints;
-        healer.attack(hero);
+        healer.heal(hero);
         assertEquals(healer.manaPoints - healer.spellCost, healer.manaPoints);
         assertEquals(HPBeforeTurn + healer.weaponDamage, hero.lifePoints);
+    }
+
+    void attack(Enemy enemy) {
+        Healer healer = new Healer();
+        int manaBeforeTurn = healer.manaPoints;
+        int HPBeforeTurn = enemy.lifePoints;
+        healer.attack(enemy);
+        assertEquals(manaBeforeTurn - healer.spellCost, healer.manaPoints);
+        assertEquals(HPBeforeTurn - healer.weaponDamage, enemy.lifePoints);
     }
 }
--- src/test/com/isep/rpg/hero/WarriorTest.java
+++ src/test/com/isep/rpg/hero/WarriorTest.java
@@ -1,7 +1,16 @@
 package com.isep.rpg.hero;
 
+import com.isep.rpg.enemy.Enemy;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarriorTest {
+
+    void attack(Enemy enemy) {
+        Warrior warrior = new Warrior();
+        int HPBeforeTurn = enemy.lifePoints;
+        warrior.attack(enemy);
+        assertEquals(HPBeforeTurn - warrior.weaponDamage, enemy.lifePoints);
+    }
 
 }
